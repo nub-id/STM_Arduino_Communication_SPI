7d7efb8f64c6a85bf69db45e471c464b2a3b42d7
Nabid <shamsesheefat-2024313762@rme.du.ac.bd>
2026-09-19 12:41:51 +0600

    STM code removed

--- STM_to_Arduino/STM32 f446re SPI/Debug/Src/STM32f446re_GPIO.d
+++
@@ -1,4 +0,0 @@
-Src/STM32f446re_GPIO.o: ../Src/STM32f446re_GPIO.c \
- ../Inc/STM32f446re_GPIO.h ../Inc/STM32_f446re.h
-../Inc/STM32f446re_GPIO.h:
-../Inc/STM32_f446re.h:
--- STM_to_Arduino/STM32 f446re SPI/Debug/Src/spi_senddata.d
+++
@@ -1,7 +0,0 @@
-Src/spi_senddata.o: ../Src/spi_senddata.c ../Inc/STM32_f446re.h \
- ../Inc/STM32f446re_GPIO.h ../Inc/STM32_f446re.h \
- ../Inc/STM32f446re_SPI_driver.h
-../Inc/STM32_f446re.h:
-../Inc/STM32f446re_GPIO.h:
-../Inc/STM32_f446re.h:
-../Inc/STM32f446re_SPI_driver.h:
